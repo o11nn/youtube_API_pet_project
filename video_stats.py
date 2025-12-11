0c32de6b823c2dd07b526def33dc95eca4083e44
Ilya Lankuts <mr.lek4r@gmail.com>
2025-12-11 19:51:44 +0100

    Create JSON file for video statistics

--- video_stats.py
+++ video_stats.py
@@ -1,7 +1,12 @@
 import requests
+import json
+
 API_KEY = "AIzaSyArVLLDC2q7bhBEBUWwpGVLmaIxPBgVqqU"
 CHANNEL_HANDLE = "MrBeast"
 url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
 response = requests.get(url)
 print(response)
+
+data = response.json()
+print(json.dumps(data, indent=4))
