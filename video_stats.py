aa9923b2f7d180ab6f6ba78ee771fc7343ad8119
Ilya Lankuts <mr.lek4r@gmail.com>
2025-12-13 13:40:53 +0100

    create the function and add .env to gitignore tereby hide the API-key

--- video_stats.py
+++ video_stats.py
@@ -1,12 +1,30 @@
 import requests
 import json
 
-API_KEY = "AIzaSyArVLLDC2q7bhBEBUWwpGVLmaIxPBgVqqU"
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
+
+API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
-url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-response = requests.get(url)
-print(response)
+def get_playlist_id():
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-data = response.json()
-print(json.dumps(data, indent=4))
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        #print(json.dumps(data, indent=4))
+
+        channel_items = data["items"][0]
+
+        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        #print(channel_playlist_id)
+
+        return channel_playlist_id
+    except requests.exceptions.RequestException as e:
+        raise e
+if __name__ == "__main__":
+    get_playlist_id()
