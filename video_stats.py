086252f3ae46533c283e9dd8e231eea08f951a4d
Ilya Lankuts <mr.lek4r@gmail.com>
2025-12-16 21:53:49 +0100

    extract all date to json file

--- video_stats.py
+++ video_stats.py
@@ -1,8 +1,8 @@
 import requests
 import json
-
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path='.env')
 
@@ -90,7 +90,14 @@ def extract_video_data(video_ids):
         return extracted_data
     except requests.exceptions.RequestException as e:
         raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
