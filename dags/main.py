35bd8e27f806566a9e6bfb0e1877eba58864e34f
o11nn <mr.lek4r@gmail.com>
2026-01-30 19:36:39 +0100

    feat: Update DAGs and add trigger operators for data processing and quality checks

--- dags/main.py
+++ dags/main.py
@@ -2,6 +2,7 @@ from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from datawarehouse.dwh import staging_table, core_table
 from data_quality.soda import yt_elt_data_quality
@@ -24,6 +25,12 @@ default_args = {
     #'end_date': datetime(2025, 12, 31, tzinfo=local_tz),
 }
 
+#variables
+staging_schema = "staging"
+core_schema = "core"
+
+# DAG 1: Produce JSON 
+
 with DAG(
     dag_id = "produce_json",
     default_args=default_args,
@@ -31,43 +38,55 @@ with DAG(
     schedule="0 14 * * *",
     catchup=False
 
-) as dag:
+) as dag_produce:
+    
     # Define the tasks using Python functions
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json = save_to_json(extract_data)
     
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+        
+    )
+    
     #dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json
+    playlist_id >> video_ids >> extract_data >> save_to_json >> trigger_update_db
 
+# DAG 2: Update DB
 with DAG(
     dag_id = "update_db",
     default_args=default_args,
     description="DAG to process JSON and insert data into both staging and core schemas",
-    schedule="0 15 * * *",
-    catchup=False
+    catchup=False,
+    schedule=None,
+    
 
-) as dag:
+) as dag_update:
     # Define the tasks using Python functions
     update_staging = staging_table()
     update_core = core_table()
-  
-    #dependencies
-    update_staging >> update_core
     
-#variables
-staging_schema = "staging"
-core_schema = "core"
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+        
+    )
+    
+    #dependencies
+    update_staging >> update_core >> trigger_data_quality
+    
     
 with DAG(
     dag_id = "data_quality",
     default_args=default_args,
     description="DAG to check the data quality on both staging and core schemas",
-    schedule="0 16 * * *",
-    catchup=False
+    catchup=False,
+    schedule=None,
 
-) as dag:
+) as dag_quality:
     # Define the tasks using Python functions
     soda_validate_staging = yt_elt_data_quality(staging_schema)
     soda_validate_core = yt_elt_data_quality(core_schema)
